383a2b88fcdf786881b93425bf74a21f57ea9ea4
liumk <liumk@mail.jj.cn>
2018-04-25 15:07:09 +0800

    Cell

--- Assets/Scripts/Game/MapInit.cs
+++ Assets/Scripts/Game/MapInit.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapInit : MonoBehaviour {
+[System.Serializable]
+public class MapInit : MonoBehaviour
+{
+    static float s23 = Mathf.Sqrt(3) * 0.5f;
     private Vector3[] getCellVertexs()
     {
         Vector3[] vertexs = new Vector3[6];
@@ -37,37 +40,76 @@ public class MapInit : MonoBehaviour {
 
         return triangles;
     }
-    GameObject createCell(Vector3 poi, Quaternion rot)
+    GameObject createCell(Vector3 poi, Quaternion rot = new Quaternion())
     {
         GameObject cell = new GameObject();
         cell.transform.position = poi;
         cell.transform.rotation = rot;
         cell.AddComponent<MeshFilter>();
         var mr = cell.AddComponent<MeshRenderer>();
-
-        
         var mat = Resources.Load<Material>("Materials/normal");
+        mat.color = m_color;
         mr.material = mat;
         cell.transform.parent = gameObject.transform;
-
         var mesh = cell.GetComponent<MeshFilter>().mesh;
         mesh.vertices = getCellVertexs();
         mesh.triangles = getTriangles();
         return cell;
     }
 	// Use this for initialization
-	void Start () {
-        s3 = Mathf.Sqrt(3);
-        s23 = s3 * 0.5f;
+	void Start ()
+    {
+        if (gameObject == null)
+        {
+            return;
+        }
+        float di = 3 + m_spacing * Mathf.Sqrt(3);
+        float dj = (Mathf.Sqrt(3) + m_spacing) * 0.5f;
+        for (int j = 0; j < m_countY; j++)
+        {
+            if (j % 2 == 0)
+            {
+                for (int i = 0; i < m_countX; i++)
+                {
+                    createCell(new Vector3(i * di, 0, j * dj));
+                }
+            }
+            else
+            {
+                for (int i = 0; i < m_countX; i++)
+                {
+                    createCell(new Vector3((i + 0.5f) * di, 0, j * dj));
+                }
+            }
+        }
         createCell(new Vector3(), new Quaternion());
+        if(m_isCenter == true)
+        {
+            if(m_countY > 1)
+            {
+                gameObject.transform.position = new Vector3(m_x - ((m_countX - 0.5f) * di * 0.5f), m_y, m_z - ((m_countY - 1) * dj * 0.5f));
+            } else
+            {
+                gameObject.transform.position = new Vector3(m_x, m_y, m_z);
+            }
+        }
+        else
+        {
+            gameObject.transform.position = new Vector3(m_x, m_y, m_z);
+        }
     }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+    }
 
-    static float s3;
-    static float s23;
-    static float s_spacing = 0.1f;
+    public int m_countX = 10;
+    public int m_countY = 20;
+    public bool m_isCenter = true;
+    public float m_spacing = 0.1f;
+    public float m_x = 0;
+    public float m_y = 0;
+    public float m_z = 0;
+    public Color m_color = Color.gray;
 }
