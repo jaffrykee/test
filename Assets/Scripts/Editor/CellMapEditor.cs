fb9237ba4700e87e527153b58930487a53c11ee5
liumk <liumk@mail.jj.cn>
2018-04-28 18:32:40 +0800

    slip

--- Assets/Scripts/Editor/CellMapEditor.cs
+++ Assets/Scripts/Editor/CellMapEditor.cs
@@ -23,6 +23,27 @@ public class CellMapEditor : EditorWindow
     private void OnEnable()
     {
         UnityEngine.Input.imeCompositionMode = IMECompositionMode.On;
+
+        m_HorizontalSplitterRect = new Rect(
+            (int)(position.x + position.width * m_HorizontalSplitterPercent),
+            position.y,
+            k_SplitterWidth,
+            position.height
+            );
+
+        m_VerticalSplitterRectRight = new Rect(
+            m_HorizontalSplitterRect.x,
+            (int)(position.y + m_HorizontalSplitterRect.height * m_VerticalSplitterPercentRight),
+            (position.width - m_HorizontalSplitterRect.width) - k_SplitterWidth,
+            k_SplitterWidth
+            );
+
+        m_VerticalSplitterRectLeft = new Rect(
+            position.x,
+            (int)(position.y + m_HorizontalSplitterRect.height * m_VerticalSplitterPercentLeft),
+            (m_HorizontalSplitterRect.width) - k_SplitterWidth,
+            k_SplitterWidth
+            );
     }
     private void resizeCellMap(int x, int y)
     {
@@ -30,16 +51,33 @@ public class CellMapEditor : EditorWindow
     }
     private void OnGUI()
     {
-        GUILayout.BeginVertical();
+        HandleHorizontalResize();
+        HandleVerticalResize();
+
+        var unitTreeRect = new Rect(
+            position.x,
+            position.y,
+            m_HorizontalSplitterRect.x,
+            m_VerticalSplitterRectLeft.y - position.y
+            );
+
         if (GUILayout.Button("刷新", ZESetting.LayoutSetting("Button")) || m_cmdTree == null)
         {
             m_cmdTree = new CellMapDataFileTreeView(new TreeViewState());
         }
-        GUILayout.BeginHorizontal();
-
         //TreeView
-        m_cmdTree.OnGUI(new Rect(0, 0, 400, 500));
+        m_cmdTree.OnGUI(unitTreeRect);
 
+        var unitPreviewRect = new Rect(
+            unitTreeRect.x,
+            unitTreeRect.y + unitTreeRect.height + k_SplitterWidth,
+            unitTreeRect.width,
+            position.height - unitTreeRect.height - k_SplitterWidth * 2
+            );
+
+        GUILayout.BeginArea(unitPreviewRect, EditorStyles.helpBox);
+        GUILayout.BeginVertical();
+        GUILayout.BeginHorizontal();
         EditorGUILayout.BeginVertical();
         EditorGUILayout.BeginHorizontal();
         EditorGUILayout.LabelField("地图尺寸：", ZESetting.LayoutSetting("LabelFieldShort"));
@@ -63,7 +101,100 @@ public class CellMapEditor : EditorWindow
         EditorGUILayout.EndVertical();
         GUILayout.EndHorizontal();
         GUILayout.EndVertical();
+        GUILayout.EndArea();
+
+
+
+        float panelLeft = m_HorizontalSplitterRect.x + k_SplitterWidth;
+        float panelWidth = m_VerticalSplitterRectRight.width - k_SplitterWidth * 2;
+        float panelHeight = m_VerticalSplitterRectRight.y - position.y;
+
+        var unitListRect = new Rect(
+            panelLeft,
+            position.y,
+            panelWidth,
+            panelHeight
+            );
+        //m_UnitList.OnGUI(unitListRect);
+        var unitEditorRect = new Rect(
+            panelLeft,
+            position.y + panelHeight + k_SplitterWidth,
+            panelWidth,
+            (position.height - panelHeight) - k_SplitterWidth * 2
+            );
+
+        GUILayout.BeginArea(unitEditorRect, EditorStyles.helpBox);
+        //m_UnitEditor.OnGUI(unitListRect);
+        GUILayout.EndArea();
+
+        if (m_ResizingHorizontalSplitter || m_ResizingVerticalSplitterLeft || m_ResizingVerticalSplitterRight)
+        {
+            Repaint();
+        }
     }
+
+    #region splitter
+    private void HandleHorizontalResize()
+    {
+        m_HorizontalSplitterRect.x = (int)(position.width * m_HorizontalSplitterPercent);
+        m_HorizontalSplitterRect.height = position.height;
+
+        EditorGUIUtility.AddCursorRect(m_HorizontalSplitterRect, MouseCursor.ResizeHorizontal);
+        if (Event.current.type == EventType.MouseDown && m_HorizontalSplitterRect.Contains(Event.current.mousePosition))
+        {
+            m_ResizingHorizontalSplitter = true;
+        }
+
+        if (m_ResizingHorizontalSplitter)
+        {
+            m_HorizontalSplitterPercent = Mathf.Clamp(Event.current.mousePosition.x / position.width, 0.1f, 0.9f);
+            m_HorizontalSplitterRect.x = (int)(position.width * m_HorizontalSplitterPercent);
+        }
+
+        if (Event.current.type == EventType.MouseUp)
+        {
+            m_ResizingHorizontalSplitter = false;
+        }
+    }
+
+    private void HandleVerticalResize()
+    {
+        m_VerticalSplitterRectRight.x = m_HorizontalSplitterRect.x;
+        m_VerticalSplitterRectRight.y = (int)(m_HorizontalSplitterRect.height * m_VerticalSplitterPercentRight);
+        m_VerticalSplitterRectRight.width = position.width - m_HorizontalSplitterRect.x;
+        m_VerticalSplitterRectLeft.y = (int)(m_HorizontalSplitterRect.height * m_VerticalSplitterPercentLeft);
+        m_VerticalSplitterRectLeft.width = m_VerticalSplitterRectRight.width;
+
+        EditorGUIUtility.AddCursorRect(m_VerticalSplitterRectRight, MouseCursor.ResizeVertical);
+        if (Event.current.type == EventType.MouseDown && m_VerticalSplitterRectRight.Contains(Event.current.mousePosition))
+        {
+            m_ResizingVerticalSplitterRight = true;
+        }
+
+        EditorGUIUtility.AddCursorRect(m_VerticalSplitterRectLeft, MouseCursor.ResizeVertical);
+        if (Event.current.type == EventType.MouseDown && m_VerticalSplitterRectLeft.Contains(Event.current.mousePosition))
+        {
+            m_ResizingVerticalSplitterLeft = true;
+        }
+
+        if (m_ResizingVerticalSplitterRight)
+        {
+            m_VerticalSplitterPercentRight = Mathf.Clamp(Event.current.mousePosition.y / m_HorizontalSplitterRect.height, 0.2f, 0.98f);
+            m_VerticalSplitterRectRight.y = (int)(m_HorizontalSplitterRect.height * m_VerticalSplitterPercentRight);
+        }
+        else if (m_ResizingVerticalSplitterLeft)
+        {
+            m_VerticalSplitterPercentLeft = Mathf.Clamp(Event.current.mousePosition.y / m_HorizontalSplitterRect.height, 0.25f, 0.98f);
+            m_VerticalSplitterRectLeft.y = (int)(m_HorizontalSplitterRect.height * m_VerticalSplitterPercentLeft);
+        }
+
+        if (Event.current.type == EventType.MouseUp)
+        {
+            m_ResizingVerticalSplitterRight = false;
+            m_ResizingVerticalSplitterLeft = false;
+        }
+    }
+    #endregion
 
     private const string c_rootPath = "./Assets/Resources/Data/CellMap/";
     private const string c_resPath = "Data/CellMap/";
@@ -76,5 +207,19 @@ public class CellMapEditor : EditorWindow
     public CellMapDataFileTreeView m_cmdTree;
 
     #region splitter
+    bool m_ResizingHorizontalSplitter = false;
+    bool m_ResizingVerticalSplitterRight = false;
+    bool m_ResizingVerticalSplitterLeft = false;
+    Rect m_HorizontalSplitterRect, m_VerticalSplitterRectRight, m_VerticalSplitterRectLeft;
+
+    [SerializeField]
+    float m_HorizontalSplitterPercent = 0.4f;
+    [SerializeField]
+    float m_VerticalSplitterPercentRight = 0.7f;
+    [SerializeField]
+    float m_VerticalSplitterPercentLeft = 0.85f;
+
+    const float k_SplitterWidth = 3f;
+    private static float m_UpdateDelay = 0f;
     #endregion
 }
