9926333c16ac861c30475154323f4f7431d78a52
liumk <liumk@mail.jj.cn>
2018-05-09 15:59:18 +0800

    height

--- Assets/Scripts/Editor/CellMapEditor.cs
+++ Assets/Scripts/Editor/CellMapEditor.cs
@@ -215,6 +215,7 @@ public class CellMapEditor : EditorWindow
                             {
                                 //normal
                                 texId |= c_cbtnStateNormal;
+                                texId = (uint)(texId + curCell.height);
                             }
                         }
                     }
